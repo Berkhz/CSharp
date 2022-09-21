041b1a506030fdfb8824e5830341b9bf75ca7899
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-21 17:57:11 -0300

    upload

---
+++ AprendendoCSharp/Variáveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Variáveis");
+
+        int idade;
+
+        idade = 18;
+
+        Console.WriteLine("Minha idade é: " + idade); 
+        idade = 18 - 4;
+
+        Console.WriteLine(idade);
+        idade = 4 * 2 - 6;
+
+        Console.WriteLine(idade);
+        idade = (4 - 2) * 3;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
---
+++ MinhaIdade/MinhaIdade/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - MinhaIdade");
+        int idade = 18;
+        Console.WriteLine("Minha idade é: " + idade);
+        Console.WriteLine("Pressione enter para sair...");
+        Console.ReadLine();
+    }
+}
