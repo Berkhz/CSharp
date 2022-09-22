6c450043f4883ebea6b501a9758ff1d7b49cb758
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-22 17:34:28 -0300

    update

--- AprendendoCSharp/variávelPontoFlutuante/Program.cs
+++ AprendendoCSharp/variávelPontoFlutuante/Program.cs
@@ -2,19 +2,18 @@
 class Programa
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
-
+    {  
+        Console.WriteLine("Projeto 4 - Variáveis Ponto Flutuante");
+            
         double salario; // variável double = recebe número quebrado
         salario = 845.77;
-        salario = 3000;
-
+            
         double idade;
         idade = 7.0 / 5;
-
+            
         Console.WriteLine(idade);
-        Console.WriteLine(salario);
-
+        Console.WriteLine("Meu salário é: " + salario);
+            
         Console.WriteLine("Tecle enter para sair...");
         Console.ReadLine();
     }
