d7be3ffc4a1443779ba921a27b83e931ae913def
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-27 17:03:41 -0300

    caracteresTexto

---
+++ AprendendoCSharp/caracteresTexto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(74 + 1);
+        Console.Write(letra);
+
+        letra = (char)(64 + 1);
+        Console.Write(letra);
+
+        letra = (char)(84 + 1);
+        Console.Write(letra);
+
+        letra = (char)(64 + 1);
+        Console.Write(letra);
+
+        letra = (char)(77 + 1);
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
--- AprendendoCSharp/conversõesTipos/Program.cs
+++ AprendendoCSharp/conversõesTipos/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 
 class Programa
-{
-    
-    static void Main(string[] args)
+{  
+    static void Main(string[] args) 
     {   
-        double salario = 783.01;
+        double salario = 783.01; // double: é a variável que recebe numéro quebrado
         Console.WriteLine("Seu salário é: R$" + salario);
 
         int salarioInteiro = (int)salario; // (int)salario: guarda a váriavel salário dentro de um número inteiro
@@ -17,9 +16,9 @@ class Programa
         short y = 1000; // short: contrário do long o short armazena pequenos caracteres na variável
         Console.WriteLine("Seu aluguel: R$" + y);
 
-        float altura = 1.70f;
+        float altura = 1.70f; 
         Console.WriteLine("Sua altura: " + altura + "cm");
-                 
+                                                            
         Console.WriteLine("Tecle enter para sair...");
         Console.ReadLine();
     }
