488152a94f3669820f05dc2f9e10a571bff65438
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-28 15:39:19 -0300

    update

--- AprendendoCSharp/condicionais/Program.cs
+++ AprendendoCSharp/condicionais/Program.cs
@@ -7,14 +7,22 @@ class Programa
         Console.WriteLine("Executando o Projeto 7 - Condicionais");
 
         int idadeZezinho = 17;
+        int qtdPessoas = 2;
 
-        if (idadeZezinho >= 18)
+        if (idadeZezinho >= 18) //if: condição "se"
         {
             Console.WriteLine("Liberado!");
         }
-        else
+        else //else: condição "se não"
         {
-            Console.WriteLine("Barrado!");
+            if (qtdPessoas > 0) 
+            {
+                Console.WriteLine("Ele está acompanhado. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Barrado!");
+            }
         }
 
         Console.WriteLine("Tecle enter para sair...");
