283806c33b81c34b1545be76dbb2c7e9ed64dac2
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-28 15:33:51 -0300

    condicionais

---
+++ AprendendoCSharp/condicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o Projeto 7 - Condicionais");
+
+        int idadeZezinho = 17;
+
+        if (idadeZezinho >= 18)
+        {
+            Console.WriteLine("Liberado!");
+        }
+        else
+        {
+            Console.WriteLine("Barrado!");
+        }
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
