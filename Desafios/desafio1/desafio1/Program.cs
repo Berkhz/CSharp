9ebe79ea40435a96b7923145cd96c6d3308df79f
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-29 17:26:47 -0300

    desafio1

---
+++ Desafios/desafio1/desafio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira seu salário");
+        Console.ReadLine();
+        double salario = 0;
+
+        if (salario >= 1900.0 && salario <= 2800.0)
+        {
+            Console.WriteLine("O IR será de 7.5% e pode deduzir na declaração o valor de R$142,00");
+        }
+        else if (salario >= 2800.01 && salario <= 3751.0)
+        {
+            Console.WriteLine("O IR será de 15% e pode deduzir na declaração o valor de R$350,00");
+        }
+        else if (salario >= 3751.01 && salario <= 4664.0)
+        {
+            Console.WriteLine("O IR será de 22.5% e pode deduzir na declaração o valor de R$636,00");
+        }
+        else
+        {
+            Console.WriteLine("Você não declara IR");
+        }
+
+
+
+
+    }
+}
