ea5aa912adf200b7a2aa4c0398416211cf7690f1
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-10-11 17:43:56 -0300

    encadeandoFor

---
+++ AprendendoCSharp/encadeandoFor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Encadeando For");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        //com break 
+        for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
+        {
+            for (int contadorColuna = 0; contadorColuna <= 9; contadorColuna++)
+            {
+                Console.Write("*");
+                if (contadorColuna >= contadorLinha)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        //sem break 
+        for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
+        {
+            for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
--- AprendendoCSharp/investimentoLongoPrazo/Program.cs
+++ AprendendoCSharp/investimentoLongoPrazo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Investindo a Longo Prazo");
+
+        double fatorRendimento = 1.005;
+        double vlrInvestido = 1000;
+
+        for (int anos = 1; anos <= 5; anos++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                vlrInvestido *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+        }
+
+        Console.WriteLine("Depois de 5 anos você acumulará " + vlrInvestido);
+
+        Console.WriteLine("Pressione enter para sair...");
+        Console.ReadLine();
+    }
+}
