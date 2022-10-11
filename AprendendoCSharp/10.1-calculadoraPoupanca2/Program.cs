a4e95710956322592a7c608c4f0a3d8c33e6627b
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-10-11 15:17:47 -0300

    calculadoraPoupanca2

---
+++ AprendendoCSharp/10.1-calculadoraPoupanca2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora de Poupança 2");
+
+        Console.WriteLine("Pressione enter para sair...");
+        Console.ReadLine();
+    }
+}
