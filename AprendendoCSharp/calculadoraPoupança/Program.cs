235721598dce39bfe177356492381325cc8e5485
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-10-11 15:04:23 -0300

    calculadoraPoupanca

--- AprendendoCSharp/calculadoraPoupança/Program.cs
+++ AprendendoCSharp/calculadoraPoupança/Program.cs
@@ -6,6 +6,28 @@ class Programa
     {
         Console.WriteLine("Calculadora Poupança");
 
+        double investimento = 1000.00;
+
+        // rendimento de 0.5% ao mês
+
+        //mês 1
+        //investimento = investimento + investimento * 0.005;
+        //mês 2
+        //investimento = investimento + investimento * 0.005;
+        //mês 3
+        //investimento = investimento + investimento * 0.005;
+        //Console.WriteLine(investimento);
+
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você acumulou R$" + investimento);
+
+            mes = mes + 1;
+        }
+
         Console.WriteLine("Tecle enter para sair...");
         Console.ReadLine();
     }
