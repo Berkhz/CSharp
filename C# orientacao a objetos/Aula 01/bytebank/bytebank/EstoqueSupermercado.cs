d2ff9e860a18e6acc3d4db2d61bbd93b99ca3d8a
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-11-25 17:44:29 -0300

    update

---
+++ C# orientacao a objetos/Aula 01/bytebank/bytebank/EstoqueSupermercado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    internal class EstoqueSupermercado
+    {
+        public string nome;
+        public string fabricante;
+        public int codigoDeBarras;
+        public double valorUnitario;
+        public double valorDeCompra;
+        public int qtdProdutoEmEstoque;
+    }
+}
