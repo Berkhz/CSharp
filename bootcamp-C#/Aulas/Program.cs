a3ddef264882957dc206bb58dcf5bb7be3b615fe
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-01-13 11:36:51 -0300

    update

---
+++ bootcamp-C#/Aulas/Program.cs
@@ -0,0 +1,125 @@
+﻿Console.WriteLine("Digite sua idade:");
+int idade = int.Parse(Console.ReadLine());
+
+if(idade >= 18)
+{
+    Console.WriteLine("Voto obrigatório!");
+} 
+else if(idade == 16 || idade == 17)
+{
+    Console.WriteLine("Voto facultativo!");
+}
+else
+{
+    Console.WriteLine("Você não pode votar ainda!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string = texto, precisa de áspas duplas
+// int = inteiro, não precisa de áspas duplas
+
+
+
+// Console.WriteLine("Digite seu nome:");
+// string nomeUsuario = Console.ReadLine();
+
+// Console.WriteLine("Digite a sua idade:");
+// int idade = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Seja bem vindo " + nomeUsuario + ", você tem " + idade + " anos.");
---
+++ bootcamp-C#/Exercícios/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Olá, qual seu nome:");
+string aluno = Console.ReadLine();
+
+Console.WriteLine(aluno + " digite a nota do 1° trimestre:");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(aluno + " agora digite a nota do 2° trimestre:");
+int nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(aluno + " por fim digite a nota do 3° trimestre:");
+int nota3 = int.Parse(Console.ReadLine());
+
+int soma = (nota1 + nota2 + nota3) / 3;
+
+if(soma >= 6)
+{
+    Console.WriteLine(aluno + " você passou, meus parabéns!");
+}
+else if(soma >= 4)
+{
+    Console.WriteLine(aluno + " ,infelizmente você ficou de recuperação!" );
+}
+else 
+{
+    Console.WriteLine(aluno + " infelizmente você reprovou!");
+}
