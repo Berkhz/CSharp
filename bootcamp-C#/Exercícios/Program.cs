91217a87c279517bc9dcc28f78f62d8f7cdb04dd
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-01-17 10:59:40 -0300

    update

--- bootcamp-C#/Exercícios/Program.cs
+++ bootcamp-C#/Exercícios/Program.cs
@@ -1,26 +1,75 @@
-﻿Console.WriteLine("Olá, qual seu nome:");
-string aluno = Console.ReadLine();
+﻿// Exercício 1 - Condições
+// Console.WriteLine("Olá, qual seu nome:");
+// string aluno = Console.ReadLine();
 
-Console.WriteLine(aluno + " digite a nota do 1° trimestre:");
-int nota1 = int.Parse(Console.ReadLine());
+// Console.WriteLine(aluno + " digite a nota do 1° trimestre:");
+// int nota1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine(aluno + " agora digite a nota do 2° trimestre:");
-int nota2 = int.Parse(Console.ReadLine());
+// Console.WriteLine(aluno + " agora digite a nota do 2° trimestre:");
+// int nota2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine(aluno + " por fim digite a nota do 3° trimestre:");
-int nota3 = int.Parse(Console.ReadLine());
+// Console.WriteLine(aluno + " por fim digite a nota do 3° trimestre:");
+// int nota3 = int.Parse(Console.ReadLine());
 
-int soma = (nota1 + nota2 + nota3) / 3;
+// int soma = (nota1 + nota2 + nota3) / 3;
 
-if(soma >= 6)
-{
-    Console.WriteLine(aluno + " você passou, meus parabéns!");
-}
-else if(soma >= 4)
-{
-    Console.WriteLine(aluno + " ,infelizmente você ficou de recuperação!" );
-}
-else 
-{
-    Console.WriteLine(aluno + " infelizmente você reprovou!");
-}
+// if(soma >= 6)
+// {
+//     Console.WriteLine(aluno + " você passou, meus parabéns!");
+// }
+// else if(soma >= 4)
+// {
+//     Console.WriteLine(aluno + " ,infelizmente você ficou de recuperação!" );
+// }
+// else 
+// {
+//     Console.WriteLine(aluno + " infelizmente você reprovou!");
+// }
+
+// Exercício 2 - Comparação OR
+// bool maiorDeIdade = false;
+// bool possuiAutorizacao = true;
+
+// if (maiorDeIdade || possuiAutorizacao)
+// {
+//     Console.WriteLine("Entrada permitida");
+// }
+// else
+// {
+//     Console.WriteLine("Entrada não permitida");
+// }
+
+//  Exercício 3 - Comparação AND
+// bool presencaMinima = true;
+// double media = 7.5;
+
+// if (presencaMinima && media >= 7)
+// {
+//     Console.WriteLine("Aprovado");
+// }
+// else 
+// {
+//     Console.WriteLine("Reprovado");
+// }
+
+// Exercício 4 - Comparação NOT
+// bool choveu = false;
+// bool estaTarde = false;
+
+// if (!choveu && !estaTarde)
+// {
+//     Console.WriteLine("Vou pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("Vou pedalar outro dia");
+// }
+
+// Exercício 5 - Estruturas de repetição
+// Console.WriteLine("Digite um número:");
+// int numero = int.Parse(Console.ReadLine());
+
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine(numero + " X " + contador + " = " + numero * contador);
+// }
