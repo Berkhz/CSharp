38868b8cf354708534d3d689456e32cd15a807f2
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-01-20 11:08:08 -0300

    update

--- AprendendoCSharp/encadeandoFor/Program.cs
+++ AprendendoCSharp/encadeandoFor/Program.cs
@@ -1,39 +1,39 @@
-﻿using System;
-class Programa
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Encadeando For");
+﻿// using System;
+// class Programa
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Encadeando For");
 
-        //*
-        //**
-        //***
-        //****
-        //*****
+//         //*
+//         //**
+//         //***
+//         //****
+//         //*****
 
-        //com break 
-        for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
-        {
-            for (int contadorColuna = 0; contadorColuna <= 9; contadorColuna++)
-            {
-                Console.Write("*");
-                if (contadorColuna >= contadorLinha)
-                    break;
-            }
-            Console.WriteLine();
-        }
+//         //com break 
+//         for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
+//         {
+//             for (int contadorColuna = 0; contadorColuna <= 9; contadorColuna++)
+//             {
+//                 Console.Write("*");
+//                 if (contadorColuna >= contadorLinha)
+//                     break;
+//             }
+//             Console.WriteLine();
+//         }
 
-        //sem break 
-        for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
-        {
-            for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
-        }
+//         //sem break 
+//         for (int contadorLinha = 0; contadorLinha <= 9; contadorLinha++)
+//         {
+//             for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+//             {
+//                 Console.Write("*");
+//             }
+//             Console.WriteLine();
+//         }
 
-        Console.WriteLine("Tecle enter para sair...");
-        Console.ReadLine();
-    }
-}
+//         Console.WriteLine("Tecle enter para sair...");
+//         Console.ReadLine();
+//     }
+// }
--- AprendendoCSharp/investimentoLongoPrazo/Program.cs
+++ AprendendoCSharp/investimentoLongoPrazo/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
-class Programa
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Projeto Investindo a Longo Prazo");
+﻿// using System;
+// class Programa
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Projeto Investindo a Longo Prazo");
 
-        double fatorRendimento = 1.005;
-        double vlrInvestido = 1000;
+//         double fatorRendimento = 1.005;
+//         double vlrInvestido = 1000;
 
-        for (int anos = 1; anos <= 5; anos++)
-        {
-            for (int mes = 1; mes <= 12; mes++)
-            {
-                vlrInvestido *= fatorRendimento;
-            }
-            fatorRendimento += 0.001;
-        }
+//         for (int anos = 1; anos <= 5; anos++)
+//         {
+//             for (int mes = 1; mes <= 12; mes++)
+//             {
+//                 vlrInvestido *= fatorRendimento;
+//             }
+//             fatorRendimento += 0.001;
+//         }
 
-        Console.WriteLine("Depois de 5 anos você acumulará " + vlrInvestido);
+//         Console.WriteLine("Depois de 5 anos você acumulará " + vlrInvestido);
 
-        Console.WriteLine("Pressione enter para sair...");
-        Console.ReadLine();
-    }
-}
+//         Console.WriteLine("Pressione enter para sair...");
+//         Console.ReadLine();
+//     }
+// }
