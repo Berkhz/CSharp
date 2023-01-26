09c8b68fcb9b7e053bac78ad8b7d25f1a970eb4c
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-01-26 16:46:03 -0300

    update

---
+++ bootcamp-C#/POO/Aulas/Program.cs
@@ -0,0 +1,15 @@
+﻿using Aulas.Models;
+
+ContaCorrente cc = new ContaCorrente(50);
+cc.Numero = 1234;
+cc.ExibirSaldo();
+
+cc.Sacar(10);
+
+cc.ExibirSaldo();
+
+// Exemplo Aula 2
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Kauan";
+// pessoa1.Idade = 19;
+// pessoa1.Apresentar();
